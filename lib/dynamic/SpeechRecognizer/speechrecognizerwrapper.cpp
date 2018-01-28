975e8b7194f89acdbf8ff189b92b209a0d9e316a
Zvyagintsev A.V <mimus.triurus@yandex.ru>
2018-01-28 11:17:43 +0300

    Небольшой рефакторинг проекта

--- lib/dynamic/SpeechRecognizer/speechrecognizerwrapper.cpp
+++ lib/dynamic/SpeechRecognizer/speechrecognizerwrapper.cpp
@@ -4,268 +4,268 @@
 
 namespace SpeechRecognizer {
 // параметры по умолчанию для speech recognizer
-    static const arg_t cont_args_def[ ] = {
-        POCKETSPHINX_OPTIONS,
-        /* Argument file. */
-        {"-argfile",
-         ARG_STRING,
-         NULL,
-         "Argument file giving extra arguments."},
-         {"-adcdev",
-         ARG_STRING,
-         NULL,
-         "Name of audio device to use for input."},
-         {"-infile",
-         ARG_STRING,
-         NULL,
-         "Audio file to transcribe."},
-         {"-inmic",
-         ARG_BOOLEAN,
-         "no",
-         "Transcribe audio from microphone."},
-         {"-time",
-         ARG_BOOLEAN,
-         "no",
-         "Print word times in file transcription."},
-         CMDLN_EMPTY_OPTION
-    };
+static const arg_t cont_args_def[ ] = {
+    POCKETSPHINX_OPTIONS,
+    /* Argument file. */
+    {"-argfile",
+     ARG_STRING,
+     NULL,
+     "Argument file giving extra arguments."},
+    {"-adcdev",
+     ARG_STRING,
+     NULL,
+     "Name of audio device to use for input."},
+    {"-infile",
+     ARG_STRING,
+     NULL,
+     "Audio file to transcribe."},
+    {"-inmic",
+     ARG_BOOLEAN,
+     "no",
+     "Transcribe audio from microphone."},
+    {"-time",
+     ARG_BOOLEAN,
+     "no",
+     "Print word times in file transcription."},
+    CMDLN_EMPTY_OPTION
+};
 
-    static const char* KWS_THRESHOLD = "-kws_threshold";
-    static const char* KEYPHRASE_SEARCH = "keyphrase_search";
+static const char* KWS_THRESHOLD = "-kws_threshold";
+static const char* KEYPHRASE_SEARCH = "keyphrase_search";
 
-    void SpeechRecognizerWrapper::eventRecognitionResult( const char *value ) {
-        if ( recognitionResult != nullptr )
-            ( *recognitionResult )( value );
+void SpeechRecognizerWrapper::eventRecognitionResult( const char *value ) {
+    if ( recognitionResult != nullptr )
+        ( *recognitionResult )( value );
+}
+
+void SpeechRecognizerWrapper::eventCrashMessage( const char *value ) {
+    if ( crashMessage != nullptr )
+        ( *crashMessage )( value );
+}
+
+void SpeechRecognizerWrapper::eventLogMessage( const char *value ) {
+    if ( logMessage != nullptr )
+        ( *logMessage )( value );
+}
+
+void SpeechRecognizerWrapper::recognizeFromMicrophone( ) {
+    if ( ( _ad = ad_open_dev( cmd_ln_str_r( _config, "-adcdev" ), ( int ) cmd_ln_float32_r( _config, "-samprate" ) ) ) == nullptr ) {
+        eventCrashMessage( "Audio device:  " + QString( cmd_ln_str_r( _config, "-adcdev" ) ).toUtf8( ) + " not found!" );
+        return;
+    }
+    if ( ad_start_rec( _ad ) < 0 ) {
+        eventCrashMessage( "Failed to start recording" );
+        return;
     }
 
-    void SpeechRecognizerWrapper::eventCrashMessage( const char *value ) {
-        if ( crashMessage != nullptr )
-            ( *crashMessage )( value );
+    if ( ps_start_utt( _ps ) < 0 ) {
+        eventCrashMessage( "Failed to start utt" );
+        return;
     }
 
-    void SpeechRecognizerWrapper::eventLogMessage( const char *value ) {
-        if ( logMessage != nullptr )
-            ( *logMessage )( value );
-    }
+    _utt_started = FALSE;
+    eventLogMessage( "Ready..." );
+}
 
-    void SpeechRecognizerWrapper::recognizeFromMicrophone( ) {
-        if ( ( _ad = ad_open_dev( cmd_ln_str_r( _config, "-adcdev" ), ( int ) cmd_ln_float32_r( _config, "-samprate" ) ) ) == nullptr ) {
-            eventCrashMessage( "Audio device:  " + QString( cmd_ln_str_r( _config, "-adcdev" ) ).toUtf8( ) + " not found!" );
-            return;
-        }
-        if ( ad_start_rec( _ad ) < 0 ) {
-            eventCrashMessage( "Failed to start recording" );
-            return;
-        }
-
-        if ( ps_start_utt( _ps ) < 0 ) {
-            eventCrashMessage( "Failed to start utt" );
-            return;
-        }
-
-        _utt_started = FALSE;
-        eventLogMessage( "Ready..." );
-    }
-
-    bool SpeechRecognizerWrapper::checkAcousticModelFiles( const char *assetsFilePath ) {
-        QFile file( assetsFilePath + ASSEST_FILE_NAME );
-        // если файла со списком файлов акустической модели не существует,
-        // то предоставяем библиотеке возможность самой проверять наличие соответствующих файлов
-        if ( !file.exists( ) ) {
-            eventLogMessage( "File 'assets.lst' not found. Don't worry." );
-            return true;
-        }
-        if ( !file.open( QFile::ReadOnly ) ) {
-            eventCrashMessage( "Can't open assets.lst file" );
-            return false;
-        }
-        while ( !file.atEnd( ) ) {
-            QString path( file.readLine( ) );
-            path.prepend( assetsFilePath );
-            path = QDir::toNativeSeparators( path );
-            // зaменить следующие две строки на регулярное выражение
-            path = path.remove('\r');
-            path = path.remove('\n');
-            if ( !QFile( path ).exists( ) ) {
-                eventCrashMessage( "Acoustic model. File " + path.toUtf8( ) + " not found!");
-                return false;
-            }
-        }
+bool SpeechRecognizerWrapper::checkAcousticModelFiles( const char *assetsFilePath ) {
+    QFile file( assetsFilePath + ASSEST_FILE_NAME );
+    // если файла со списком файлов акустической модели не существует,
+    // то предоставяем библиотеке возможность самой проверять наличие соответствующих файлов
+    if ( !file.exists( ) ) {
+        eventLogMessage( "File 'assets.lst' not found. Don't worry." );
         return true;
     }
-
-    void SpeechRecognizerWrapper::freeAllResources( ) {
-        if ( _ad != nullptr )
-            ad_close( _ad );
-        if ( _ps != nullptr )
-            ps_free( _ps );
-        if ( _config != nullptr )
-            cmd_ln_free_r( _config );
+    if ( !file.open( QFile::ReadOnly ) ) {
+        eventCrashMessage( "Can't open assets.lst file" );
+        return false;
     }
-
-    SpeechRecognizerWrapper::~SpeechRecognizerWrapper( ) {
-        freeAllResources( );
-    }
-
-    bool SpeechRecognizerWrapper::runRecognizerSetup( const char *destination ) {
-        QString hmmDest = QDir::toNativeSeparators( destination );
-
-        freeAllResources( );
-
-        if ( !checkAcousticModelFiles( destination ) )
+    while ( !file.atEnd( ) ) {
+        QString path( file.readLine( ) );
+        path.prepend( assetsFilePath );
+        path = QDir::toNativeSeparators( path );
+        // зaменить следующие две строки на регулярное выражение
+        path = path.remove( '\r' );
+        path = path.remove( '\n' );
+        if ( !QFile( path ).exists( ) ) {
+            eventCrashMessage( "Acoustic model. File " + path.toUtf8( ) + " not found!");
             return false;
+        }
+    }
+    return true;
+}
 
-        _config = cmd_ln_init( nullptr, cont_args_def, TRUE,
-                "-hmm", hmmDest.toUtf8( ).data( ),
-                "-remove_noise", "yes",
-                "-inmic", "yes",
-                "-adcdev", _inputDeviceName.toUtf8( ).data( ),
-                nullptr );
+void SpeechRecognizerWrapper::freeAllResources( ) {
+    if ( _ad != nullptr )
+        ad_close( _ad );
+    if ( _ps != nullptr )
+        ps_free( _ps );
+    if ( _config != nullptr )
+        cmd_ln_free_r( _config );
+}
 
+SpeechRecognizerWrapper::~SpeechRecognizerWrapper( ) {
+    freeAllResources( );
+}
+
+bool SpeechRecognizerWrapper::runRecognizerSetup( const char *destination ) {
+    QString hmmDest = QDir::toNativeSeparators( destination );
+
+    freeAllResources( );
+
+    if ( !checkAcousticModelFiles( destination ) )
+        return false;
+
+    _config = cmd_ln_init( nullptr, cont_args_def, TRUE,
+                           "-hmm", hmmDest.toUtf8( ).data( ),
+                           "-remove_noise", "yes",
+                           "-inmic", "yes",
+                           "-adcdev", _inputDeviceName.toUtf8( ).data( ),
+                           nullptr );
+    if ( _useKeyword )
         cmd_ln_set_float_r( _config, KWS_THRESHOLD, _threshold );
-        if ( _logIntoFile ) {
-            QString logDest = destination;
-            logDest.append( QDate::currentDate(  ).toString( "dd_MM_yy" ) +  ".log" );
-            logDest = QDir::toNativeSeparators( logDest );
-            cmd_ln_set_str_r( _config, "-logfn", logDest.toUtf8( ).data( ) );
-        }
+    if ( _logIntoFile ) {
+        QString logDest = destination;
+        logDest.append( QDate::currentDate(  ).toString( "dd_MM_yy" ) +  ".log" );
+        logDest = QDir::toNativeSeparators( logDest );
+        cmd_ln_set_str_r( _config, "-logfn", logDest.toUtf8( ).data( ) );
+    }
 
-        char const *cfg;
+    char const *cfg;
 
-        if ( _config && ( cfg = cmd_ln_str_r( _config, "-argfile" ) ) != nullptr )
-            _config = cmd_ln_parse_file_r( _config, cont_args_def, cfg, FALSE );
+    if ( _config && ( cfg = cmd_ln_str_r( _config, "-argfile" ) ) != nullptr )
+        _config = cmd_ln_parse_file_r( _config, cont_args_def, cfg, FALSE );
 
-        _ps = ps_init( _config );
+    _ps = ps_init( _config );
 
-        if (_ps == nullptr ) {
-            cmd_ln_free_r( _config );
-            eventCrashMessage( "error on init speechRecognizer!!!" );
-            return false;
-        }
+    if ( _ps == nullptr ) {
+        cmd_ln_free_r( _config );
+        eventCrashMessage( "error on init speechRecognizer!!!" );
+        return false;
+    }
+    return true;
+}
+
+void SpeechRecognizerWrapper::setBaseGrammar( const char *grammarName ) {
+    _baseGrammarName = grammarName;
+}
+
+void SpeechRecognizerWrapper::setKeyword( const char *keyword ) {
+    _useKeyword = true;
+    ps_set_keyphrase( _ps, KEYPHRASE_SEARCH, keyword );
+}
+
+void SpeechRecognizerWrapper::setThreshold( const double threshold ) {
+    _threshold = threshold;
+}
+
+void SpeechRecognizerWrapper::switchGrammar( const char *grammarName ) {
+    if ( _ps == nullptr ) return;
+    int result = ps_set_search( _ps, grammarName );
+    if ( result != 0 )
+        eventCrashMessage( "error on switch search:" + QString( grammarName ).toUtf8( ) );
+    else
+        eventLogMessage( "switch grammar:" + QString( grammarName ).toUtf8( ) );
+}
+
+void SpeechRecognizerWrapper::setSearchKeyword( ) {
+    if ( ( _ps != nullptr ) & ( _useKeyword ) )
+        ps_set_search( _ps, KEYPHRASE_SEARCH );
+}
+
+bool SpeechRecognizerWrapper::addGrammar( const char *grammarName , const char *grammarFileName ) {
+    if ( _ps == nullptr ) return false;
+    int result = ps_set_jsgf_file( _ps, grammarName, grammarFileName );
+    if ( result != 0 ) {
+        eventCrashMessage( "error on add grammar file:" + QString( grammarFileName ).toUtf8( ) );
+        return false;
+    }
+    else {   // инициализируем базовую грамматику по умолчанию первой успешно добавленной в декодер
+        if ( _baseGrammarName == "" )
+            _baseGrammarName = grammarName;
         return true;
     }
+}
 
-    void SpeechRecognizerWrapper::setBaseGrammar( const char *grammarName ) {
-        _baseGrammarName = grammarName;
+bool SpeechRecognizerWrapper::addGrammarString( const char *grammarName , const char *grammarString ) {
+    if ( _ps == nullptr ) return false;
+    int result = ps_set_jsgf_string( _ps, grammarName, grammarString );
+
+    if ( result != 0 ) {
+        eventCrashMessage( "Error on add grammar string:" + QString( grammarString ).toUtf8( ) );
+        return false;
+    }
+    else {   // инициализируем базовую грамматику по умолчанию первой успешно добавленной в декодер
+        if ( _baseGrammarName == "" )
+            _baseGrammarName = grammarName;
+        return true;
+    }
+}
+
+bool SpeechRecognizerWrapper::addWordIntoDictionary( const char *word, const char *phones ) {
+    if ( ps_lookup_word( _ps, word ) == NULL ) {
+        return ( ps_add_word( _ps, word, phones, 1 ) >= 0);
+    }
+    else {
+        eventCrashMessage( "Error on add word into dictionary:" + QString( word ).toUtf8( ) + " with phones:" + QString( phones ).toUtf8( ) );
+        return false;
+    }
+}
+
+void SpeechRecognizerWrapper::startListening( ) {
+    eventLogMessage( "Start listening" );
+    if ( _useKeyword ) {
+        eventLogMessage( "Set search keyword" );
+        ps_set_search( _ps, KEYPHRASE_SEARCH );
+    }
+    else {
+        ps_set_search( _ps, _baseGrammarName.toUtf8( ).data( ) );
+        eventLogMessage( QString( "Set search:" + _baseGrammarName ).toUtf8( ) );
     }
 
-    void SpeechRecognizerWrapper::setKeyword( const char *keyword ) {
-        _useKeyword = true;
-        ps_set_keyphrase( _ps, KEYPHRASE_SEARCH, keyword );
+    recognizeFromMicrophone( );
+}
+
+void SpeechRecognizerWrapper::stopListening( ) {
+    eventLogMessage( "Stop listening" );
+    ad_stop_rec( _ad );
+}
+
+void SpeechRecognizerWrapper::readMicrophoneBuffer( ) {
+    if ( _ad == nullptr ) return;
+    int32 k;
+    int16 buffer[ 2048 ];
+    uint8 in_speech;
+
+    if ( ( k = ad_read( _ad, buffer, 2048 ) ) < 0 ) {
+        eventCrashMessage( "Failed to read audio" );
+        return;
     }
+    ps_process_raw( _ps, buffer, k, FALSE, FALSE );
 
-    void SpeechRecognizerWrapper::setThreshold( const double threshold ) {
-        _threshold = threshold;
+    in_speech = ps_get_in_speech( _ps );
+    if ( in_speech && !_utt_started ) {
+        _utt_started = TRUE;
+        //eventLogMessage("Listening...");
     }
-
-    void SpeechRecognizerWrapper::switchGrammar( const char *grammarName ) {
-        if ( _ps == nullptr ) return;
-        int result = ps_set_search( _ps, grammarName );
-        if ( result != 0 )
-            eventCrashMessage( "error on switch search:" + QString( grammarName ).toUtf8( ) );
-        else
-            eventLogMessage( "switch grammar:" + QString( grammarName ).toUtf8( ) );
-    }
-
-    void SpeechRecognizerWrapper::setSearchKeyword( ) {
-        if ( ( _ps != nullptr ) && ( _useKeyword ) )
-            ps_set_search( _ps, KEYPHRASE_SEARCH );
-    }
-
-    bool SpeechRecognizerWrapper::addGrammar( const char *grammarName , const char *grammarFileName ) {
-        if ( _ps == nullptr ) return false;
-        int result = ps_set_jsgf_file( _ps, grammarName, grammarFileName );
-        if ( result != 0 ) {
-            eventCrashMessage( "error on add grammar file:" + QString( grammarFileName ).toUtf8( ) );
-            return false;
-        }
-        else {   // инициализируем базовую грамматику по умолчанию первой успешно добавленной в декодер
-            if ( _baseGrammarName == "" )
-                _baseGrammarName = grammarName;
-            return true;
-        }
-    }
-
-    bool SpeechRecognizerWrapper::addGrammarString( const char *grammarName , const char *grammarString ) {
-        if ( _ps == nullptr ) return false;
-        int result = ps_set_jsgf_string( _ps, grammarName, grammarString );
-
-        if ( result != 0 ) {
-            eventCrashMessage( "Error on add grammar string:" + QString( grammarString ).toUtf8( ) );
-            return false;
-        }
-        else {   // инициализируем базовую грамматику по умолчанию первой успешно добавленной в декодер
-            if ( _baseGrammarName == "" )
-                _baseGrammarName = grammarName;
-            return true;
-        }
-    }
-
-    bool SpeechRecognizerWrapper::addWordIntoDictionary( const char *word, const char *phones ) {
-        if ( ps_lookup_word( _ps, word ) == NULL ) {
-            return ( ps_add_word( _ps, word, phones, 1 ) >= 0);
-        }
-        else {
-            eventCrashMessage( "Error on add word into dictionary:" + QString( word ).toUtf8( ) + " with phones:" + QString( phones ).toUtf8( ) );
-            return false;
-        }
-    }
-
-    void SpeechRecognizerWrapper::startListening( ) {
-        eventLogMessage( "Start listening" );
-        if ( _useKeyword ) {
-            eventLogMessage( "Set search keyword" );
-            ps_set_search( _ps, KEYPHRASE_SEARCH );
-        }
-        else {
-            ps_set_search( _ps, _baseGrammarName.toUtf8( ).data( ) );
-            eventLogMessage( QString( "Set search:" + _baseGrammarName ).toUtf8( ) );
+    if ( !in_speech && _utt_started ) {
+        ps_end_utt( _ps );
+        char const *hyp = ps_get_hyp( _ps, nullptr );
+        if ( hyp != nullptr ) {
+            eventRecognitionResult( hyp );
         }
 
-        recognizeFromMicrophone( );
-    }
-
-    void SpeechRecognizerWrapper::stopListening( ) {
-        eventLogMessage( "Stop listening" );
-        ad_stop_rec( _ad );
-    }
-
-    void SpeechRecognizerWrapper::readMicrophoneBuffer( ) {
-        if ( _ad == nullptr ) return;
-        int32 k;
-        int16 buffer[ 2048 ];
-        uint8 in_speech;
-
-        if ( ( k = ad_read( _ad, buffer, 2048 ) ) < 0 ) {
-            eventCrashMessage( "Failed to read audio" );
-            return;
+        if ( ps_start_utt( _ps ) < 0) {
+            eventCrashMessage( "Failed to start utt" );
         }
-        ps_process_raw( _ps, buffer, k, FALSE, FALSE );
-
-        in_speech = ps_get_in_speech( _ps );
-        if ( in_speech && !_utt_started ) {
-            _utt_started = TRUE;
-            //eventLogMessage("Listening...");
-        }
-        if ( !in_speech && _utt_started ) {
-            ps_end_utt( _ps );
-            char const *hyp = ps_get_hyp( _ps, nullptr );
-            if ( hyp != nullptr ) {
-                eventRecognitionResult( hyp );
-            }
-
-            if ( ps_start_utt( _ps ) < 0) {
-                eventCrashMessage( "Failed to start utt" );
-            }
-            _utt_started = FALSE;
-        }
+        _utt_started = FALSE;
     }
+}
 
-    void SpeechRecognizerWrapper::saveLogIntoFile( bool value ) {
-       _logIntoFile = value;
-    }
+void SpeechRecognizerWrapper::saveLogIntoFile( bool value ) {
+    _logIntoFile = value;
+}
 
-    void SpeechRecognizerWrapper::setInputDeviceName( const char* name ) {
-        _inputDeviceName = name;
-    }
+void SpeechRecognizerWrapper::setInputDeviceName( const char* name ) {
+    _inputDeviceName = name;
+}
 }
--- lib/dynamic/SpeechRecognizer/speechrecognizerwrapper.h
+++ lib/dynamic/SpeechRecognizer/speechrecognizerwrapper.h
@@ -19,129 +19,128 @@ namespace SpeechRecognizer {
  * @author mimus.triurus@yandex.ru
  * @version 1.0
  */
-class SpeechRecognizerWrapper
-    {
-        ps_decoder_t *_ps;
-        cmd_ln_t *_config;
-        ad_rec_t *_ad;
-        uint8 _utt_started;
+class SpeechRecognizerWrapper {
 
-        bool _logIntoFile = true;
-        bool _useKeyword = false;
+public:
+    SpeechRecognizerWrapper( ) { }
+    ~SpeechRecognizerWrapper( );
+    /**
+    * @brief настройка распознавания голоса
+    * @param destination путь к папке где содержится акустическая модель, словарь и грамматика
+    */
+    bool runRecognizerSetup( const char *destination );
+    /**
+    * @brief устанавливаем имя грамматики поумолчанию
+    * @param grammarName имя грамматики
+    */
+    void setBaseGrammar( const char *grammarName );
+    /**
+    * @brief устанавливаем ключевое слово(OK GOOGLE!)
+    * @param keyword ключевое слово
+    */
+    void setKeyword( const char *keyword );
+    /**
+    * @brief устанавливаем порог сабатывания для ключевого слова
+    * @param threshold чем больше значение тем меньше ложных срабатываний
+    */
+    void setThreshold( const double threshold );
+    /**
+    * @brief меняем грамматику (слова для распознавания)
+    * @param grammarName имя грамматики
+    */
+    void switchGrammar( const char *grammarName );
+    /**
+    * @brief инициируем поиск ключевого слова (OK GOOGLE!)
+    */
+    void setSearchKeyword( );
+    /**
+    * @brief добавляем файл грамматики
+    * @param grammarName идентификатор грамматики
+    * @param grammarFileName имя файла грамматики
+    * файл должен лежать в <destination>/grammarFiles/
+    * где <destination> - параметр метода 'runRecognizerSetup'
+    * @return результат добавления файла грамматики (удачно\нет)
+    */
+    bool addGrammar( const char* grammarName, const char *grammarFileName );
+    /**
+    * @brief addGrammarString
+    * @param grammarName идентификатор грамматики
+    * @param grammarString формализованная строка с грамматикой
+    * @return результат добавления строки с грамматикой (удачно\нет)
+    */
+    bool addGrammarString( const char *grammarName, const char *grammarString );
+    /**
+    * @brief добавляем слово в словарь
+    * @param word слово
+    * @param phones транскрипция слова
+    * @return результат добавления - удачно\нет
+    */
+    bool addWordIntoDictionary(const char *word, const char *phones );
+    /**
+    * @brief слушаем микрофон
+    */
+    void startListening( );
+    /**
+    * @brief окончание прослушки микрофона
+    */
+    void stopListening( );
+    /**
+    * @brief читаем буффер микрофона (с перерывом в 100 мс)
+    */
+    void readMicrophoneBuffer( );
+    /**
+    * @brief сохраняем лог с подробной информацией
+    * о работе библиотеки распознвания
+    * в файл или выводим в консоль
+    * @param value в файл - true \ в консоль - false
+    */
+    void saveLogIntoFile( bool value = false );
+    /**
+    * @brief устанавливаем имя устройства ввода(микрофона)
+    * @param name имя устройства ввода(микрофона)
+    */
+    void setInputDeviceName( const char *name );
+    /**
+    * указатель на функцию-приемник результатов распознавания
+    */
+    void ( *recognitionResult )( const char *value );
+    /**
+    * указатель на функцию-приемник сообщений об ошибках
+    */
+    void ( *crashMessage )( const char *value );
+    /**
+    * указатель на функцию-приемник отладочных сообщений
+    */
+    void ( *logMessage )( const char *value );
+private:
+    ps_decoder_t *_ps;
+    cmd_ln_t *_config;
+    ad_rec_t *_ad;
+    uint8 _utt_started;
 
-        double _threshold = 1e+10f;
+    bool _logIntoFile{ true };
+    bool _useKeyword{ false };
 
-        QString _baseGrammarName = "";
-        QString _inputDeviceName = "sysdefault";
+    double _threshold{ 1e+10f };
 
-        const QString ASSEST_FILE_NAME = "assets.lst";
+    QString _baseGrammarName{ "" };
+    QString _inputDeviceName{ "sysdefault" };
 
-        void eventRecognitionResult( const char* value );
+    const QString ASSEST_FILE_NAME{ "assets.lst" };
 
-        void eventCrashMessage( const char* value );
+    void eventRecognitionResult( const char* value );
 
-        void eventLogMessage( const char* value );
+    void eventCrashMessage( const char* value );
 
-        void recognizeFromMicrophone( );
+    void eventLogMessage( const char* value );
 
-        bool checkAcousticModelFiles( const char* assetsFilePath );
+    void recognizeFromMicrophone( );
 
-        void freeAllResources( );
-    public:
-        /**
-         * @brief конструктор
-         */
-         SpeechRecognizerWrapper( ) { }
-         ~SpeechRecognizerWrapper( );
-        /**
-         * @brief настройка распознавания голоса
-         * @param destination путь к папке где содержится акустическая модель, словарь и грамматика
-         */
-        bool runRecognizerSetup( const char *destination );
-        /**
-         * @brief устанавливаем имя грамматики поумолчанию
-         * @param grammarName имя грамматики
-         */
-        void setBaseGrammar( const char *grammarName );
-        /**
-         * @brief устанавливаем ключевое слово(OK GOOGLE!)
-         * @param keyword ключевое слово
-         */
-        void setKeyword( const char *keyword );
-        /**
-         * @brief устанавливаем порог сабатывания для ключевого слова
-         * @param threshold чем больше значение тем меньше ложных срабатываний
-         */
-        void setThreshold( const double threshold );
-        /**
-         * @brief меняем грамматику (слова для распознавания)
-         * @param grammarName имя грамматики
-         */
-        void switchGrammar( const char *grammarName );
-        /**
-         * @brief инициируем поиск ключевого слова (OK GOOGLE!)
-         */
-        void setSearchKeyword( );
-        /**
-         * @brief добавляем файл грамматики
-         * @param grammarName идентификатор грамматики
-         * @param grammarFileName имя файла грамматики
-         * файл должен лежать в <destination>/grammarFiles/
-         * где <destination> - параметр метода 'runRecognizerSetup'
-         * @return результат добавления файла грамматики (удачно\нет)
-         */
-        bool addGrammar( const char* grammarName, const char *grammarFileName );
-        /**
-         * @brief addGrammarString
-         * @param grammarName идентификатор грамматики
-         * @param grammarString формализованная строка с грамматикой
-         * @return результат добавления строки с грамматикой (удачно\нет)
-         */
-        bool addGrammarString( const char *grammarName, const char *grammarString );
-        /**
-         * @brief добавляем слово в словарь
-         * @param word слово
-         * @param phones транскрипция слова
-         * @return результат добавления - удачно\нет
-         */
-        bool addWordIntoDictionary(const char *word, const char *phones );
-        /**
-         * @brief слушаем микрофон
-         */
-        void startListening( );
-        /**
-         * @brief окончание прослушки микрофона
-         */
-        void stopListening( );
-        /**
-         * @brief читаем буффер микрофона (с перерывом в 100 мс)
-         */
-        void readMicrophoneBuffer( );
-        /**
-         * @brief сохраняем лог с подробной информацией
-         * о работе библиотеки распознвания
-         * в файл или выводим в консоль
-         * @param value в файл - true \ в консоль - false
-         */
-        void saveLogIntoFile( bool value = false );
-        /**
-         * @brief устанавливаем имя устройства ввода(микрофона)
-         * @param name имя устройства ввода(микрофона)
-         */
-        void setInputDeviceName( const char *name );
-        /**
-         * указатель на функцию-приемник результатов распознавания
-         */
-        void ( *recognitionResult )( const char *value );
-        /**
-         * указатель на функцию-приемник сообщений об ошибках
-         */
-        void ( *crashMessage )( const char *value );
-        /**
-         * указатель на функцию-приемник отладочных сообщений
-         */
-        void ( *logMessage )( const char *value );
-    };
+    bool checkAcousticModelFiles( const char* assetsFilePath );
+
+    void freeAllResources( );
+};
+
 }
 
 #endif // SPEECHRECOGNIZERWRAPPER_H
